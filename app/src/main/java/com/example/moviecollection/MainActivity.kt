0e2cbe2611d95a332c671c92dc7b7bb6987d6c5f
RadyshKV <radysh@inbox.ru>
2021-04-28 01:10:39 +0300

    Home Work 1

--- app/src/main/java/com/example/moviecollection/MainActivity.kt
+++ app/src/main/java/com/example/moviecollection/MainActivity.kt
@@ -1,14 +1,44 @@
-package com.example.moviecollection;
+package com.example.moviecollection
 
-import androidx.appcompat.app.AppCompatActivity;
+import Movie
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    private fun initView() {
+        findViewById<Button>(R.id.button).setOnClickListener { testFun() }
+    }
+
+
+    private fun testFun() {
+        val movie = Movie("Операция Ы", "Гайдай")
+        val movieCopy = movie.copy()
+        Toast.makeText(this, movieCopy.toString(), Toast.LENGTH_SHORT).show()
+        val movieList = ArrayList<Movie>(3)
+        movieList.add(Movie("первый", "1111"))
+        movieList.add(Movie("второй", "2222"))
+        movieList.add(Movie("третий", "3333"))
+        for (movies in movieList) {
+            println(movies.title)
+        }
+
+        for (i in 0..2 step 2) {
+            println(movieList.get(i).producer)
+        }
+
+        for (i in 0 until movieList.size) {
+            println(movieList.get(i).toString())
+        }
     }
 }
+
